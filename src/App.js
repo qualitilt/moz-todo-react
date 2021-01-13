fbc40b621cab3b6bd9223f43cc59eb89633798eb
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2021-01-13 15:12:52 +0100

    test:Variables and props

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const subject = props.subject
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Hello {subject}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App subject= "ReactJS" />
   </React.StrictMode>,
   document.getElementById('root')
 );
